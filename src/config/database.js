935bd9a0820191f7e8cb3eb171a2e5dfafde1498
Axlslz <axlsalazarr1602@gmail.com>
2024-10-06 21:44:18 -0600

    remastered

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST || '127.0.0.1',  
-  user: process.env.DB_USER || 'root',       
-  password: process.env.DB_PASS || '', 
-  database: process.env.DB_NAME || 'agentelite', 
-  port: process.env.DB_PORT || 3306
+  host: process.env.DB_HOST, 
+    user: process.env.DB_USER, 
+    password: process.env.DB_PASS, 
+    database: process.env.DB_NAME, 
+    port: process.env.DB_PORT,
 });
 
 connection.connect((err) => {
