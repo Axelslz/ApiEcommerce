0eb54395a0bf4e6001a3af08f4a76a7297e6b3d4
Axlslz <axlsalazarr1602@gmail.com>
2024-10-06 21:56:39 -0600

    .

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-    host: '127.0.0.1', 
+    host: process.env.DB_HOST, 
     user: process.env.DB_USER, 
     password: process.env.DB_PASS, 
     database: process.env.DB_NAME, 
