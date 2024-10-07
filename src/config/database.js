7c3baf57e4658a5a8eae53e4b3d45e74dbf382aa
Axlslz <axlsalazarr1602@gmail.com>
2024-10-06 22:41:25 -0600

    config database

--- src/config/database.js
+++ src/config/database.js
@@ -2,11 +2,11 @@ const mysql = require('mysql');
 require('dotenv').config();
 
 const connection = mysql.createConnection({
-    host: process.env.DB_HOST, 
-    user: process.env.DB_USER, 
-    password: process.env.DB_PASS, 
-    database: process.env.DB_NAME, 
-    port: process.env.DB_PORT || 3306
+  host: process.env.DB_HOST || 'apiecommerce.railway.internal',
+  user: process.env.DB_USER || 'agent',
+  password: process.env.DB_PASSWORD || 'liteA4231',
+  database: process.env.DB_NAME || 'agentelite',
+  port: process.env.DB_PORT || 3306
 });
 
 connection.connect((err) => {
