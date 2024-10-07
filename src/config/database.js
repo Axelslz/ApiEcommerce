d0a8ee8c9f3c1407ada7de22641550076b38fd76
Axlslz <axlsalazarr1602@gmail.com>
2024-10-06 22:16:49 -0600

    .

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,11 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: 'apiecommerce.railway.internal',  
-  user: 'agent',                         
-  password: 'liteA4231',                 
-  database: 'agentelite',                 
-  port: 3306                              
+    host: process.env.DB_HOST, 
+    user: process.env.DB_USER, 
+    password: process.env.DB_PASS, 
+    database: process.env.DB_NAME, 
+    port: process.env.DB_PORT,
 });
 
 connection.connect((err) => {
